ae8dfbc7e32323636749e1ad09db922cba22de66
Prater448 <77420141+Prater448@users.noreply.github.com>
2021-04-21 19:27:01 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,39 @@
+//variables go up here
+let img1;
+let img2;
+let img3;
+let sound;
+let vid1;
+
+function preload(){
+  img1 = loadImage('assets/burn.jpg');
+  img2 = loadImage('assets/done.jpg');
+  img3 = loadImage('assets/poepol.jpg');
+  sound = loadSound('assets/walk.mp3');
+  vid1 = createVideo('assets/');
+  //stuff to prepare gets done here
+}
+
+function setup() {
+  createCanvas(1920, 1080);
+//things to establish the "sandbox" go here
+}
+
+function draw() {
+  background(255, 204, 0);
+  rect(0,0,700,500);
+  image(img1, 0, 0);
+  image(img2, 450, 300);
+  image(img3, 1500, 100);
+  //think of this area as a looping function that continuall puts things on screen, in order
+}
+
+function mousePressed() {
+  //what happens when I click?
+  if (sound.isPlaying()) {
+    sound.stop();
+  }
+  else {
+    sound.play();
+    }
+  }
